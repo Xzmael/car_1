037ea9384d277075d1fbab1451209883ef7234a2
Xzmael <1412908411@qq.com>
2026-07-25 17:24:01 +0800

    Reduce yaw hold sensitivity

--- car_1_1/Hardware/motor.c
+++ car_1_1/Hardware/motor.c
@@ -5,9 +5,9 @@
 
 #define MOTOR_PWM_PERIOD       (1600U)
 #define MOTOR_MAX_DUTY         (100U)
-#define MOTOR_MAX_DIFFERENTIAL (5.0f)
-#define MOTOR_YAW_P_GAIN       (1.0f)
-#define MOTOR_YAW_DEADBAND     (1.5f)
+#define MOTOR_MAX_DIFFERENTIAL (2.0f)
+#define MOTOR_YAW_P_GAIN       (0.30f)
+#define MOTOR_YAW_DEADBAND     (4.0f)
 #define MOTOR_YAW_JUMP_LIMIT   (15.0f)
 
 static Motor_Status motorStatus;
