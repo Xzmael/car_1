19bd4f5167b41e01fd81eca324f3a3b4b6a700b8
Xzmael <1412908411@qq.com>
2026-07-27 23:40:15 +0800

    Exclude preserved OLED driver from TFT build

---
+++ car_1_1/Debug/Hardware/subdir_vars.mk
@@ -0,0 +1,62 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Hardware/buzzer.c \
+../Hardware/gray.c \
+../Hardware/key.c \
+../Hardware/led.c \
+../Hardware/motor.c \
+../Hardware/tft.c \
+../Hardware/vision_uart.c 
+
+C_DEPS += \
+./Hardware/buzzer.d \
+./Hardware/gray.d \
+./Hardware/key.d \
+./Hardware/led.d \
+./Hardware/motor.d \
+./Hardware/tft.d \
+./Hardware/vision_uart.d 
+
+OBJS += \
+./Hardware/buzzer.o \
+./Hardware/gray.o \
+./Hardware/key.o \
+./Hardware/led.o \
+./Hardware/motor.o \
+./Hardware/tft.o \
+./Hardware/vision_uart.o 
+
+OBJS__QUOTED += \
+"Hardware\buzzer.o" \
+"Hardware\gray.o" \
+"Hardware\key.o" \
+"Hardware\led.o" \
+"Hardware\motor.o" \
+"Hardware\tft.o" \
+"Hardware\vision_uart.o" 
+
+C_DEPS__QUOTED += \
+"Hardware\buzzer.d" \
+"Hardware\gray.d" \
+"Hardware\key.d" \
+"Hardware\led.d" \
+"Hardware\motor.d" \
+"Hardware\tft.d" \
+"Hardware\vision_uart.d" 
+
+C_SRCS__QUOTED += \
+"../Hardware/buzzer.c" \
+"../Hardware/gray.c" \
+"../Hardware/key.c" \
+"../Hardware/led.c" \
+"../Hardware/motor.c" \
+"../Hardware/tft.c" \
+"../Hardware/vision_uart.c" 
+
+
